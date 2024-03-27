e990518684590e49fc1c1f9ac83de7e1fdf12403
thinun <75312054+thinun@users.noreply.github.com>
2024-03-27 11:14:06 +0530

    1st

--- Text To Speech/main.py
+++ Text To Speech/main.py
@@ -1,1 +1,9 @@
-print('Hello World!')
+from gtts import gTTS
+
+text = 'Hello World! My name is Thinun Tharuhsika'
+language = 'en'
+
+txt_1 = gTTS(text=text, lang=language, slow=False)
+txt_1.save('hello_world_2.mp3')
+
+
